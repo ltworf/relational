c658041a7dff41ab639f755742b6f492948a0a55
LtWorf <LtWorf@014f5005-505e-4b48-8d0a-63407b615a7c>
2009-02-13 15:52:34 +0000

    Changed event to show fields of relations in the sidebar since it was behaving differently depending on the used theme
    git-svn-id: http://galileo.dmi.unict.it/svn/relational/trunk@85 014f5005-505e-4b48-8d0a-63407b615a7c

--- maingui.py
+++ maingui.py
@@ -403,7 +403,7 @@ class Ui_Form(object):
         QtCore.QObject.connect(self.cmdInsert,QtCore.SIGNAL("clicked()"),self.insertTuple)
         QtCore.QObject.connect(self.cmdDelete,QtCore.SIGNAL("clicked()"),self.deleteTuple)
         QtCore.QObject.connect(self.lstRelations,QtCore.SIGNAL("itemDoubleClicked(QListWidgetItem*)"),self.printRelation)
-        QtCore.QObject.connect(self.lstRelations,QtCore.SIGNAL("itemActivated(QListWidgetItem*)"),self.showAttributes)
+        QtCore.QObject.connect(self.lstRelations,QtCore.SIGNAL("itemClicked(QListWidgetItem*)"),self.showAttributes)
         QtCore.QMetaObject.connectSlotsByName(Form)
         Form.setTabOrder(self.    txtResult,self.txtQuery)
         Form.setTabOrder(self.txtQuery,self.cmdExecute)
