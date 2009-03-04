1d59ca0b629ea7c56313126592c34c3e0b196ea0
LtWorf <LtWorf@014f5005-505e-4b48-8d0a-63407b615a7c>
2009-03-04 21:25:54 +0000

    removed useless dependences
    git-svn-id: http://galileo.dmi.unict.it/svn/relational/trunk@98 014f5005-505e-4b48-8d0a-63407b615a7c

--- debscript/gencontrol.sh
+++ debscript/gencontrol.sh
@@ -4,7 +4,7 @@
 	echo "Architecture: all"
 	echo "Maintainer: Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>"
 	echo "Installed-Size: "`du -s --apparent-size data/ | cut -f1`
-	echo "Depends: python-qt4 (>= 4.0.1-5), python (>= 2.3), debhelper (>= 5.0.38), python-central (>= 0.5.6), ttf-dejavu-core (>= 2.25-3)"
+	echo "Depends: python-qt4 (>= 4.0.1-5), python (>= 2.3), ttf-dejavu-core (>= 2.25-3)"
 	echo "Recommends: libqt4-webkit (>= 4.4.3-1)"
 	echo "Section: devel"
 	echo "Priority: optional"
