5dead3a058239c9d5ff30fec32857b7216589232
LtWorf <LtWorf@014f5005-505e-4b48-8d0a-63407b615a7c>
2009-05-13 15:39:19 +0000

    stub for parallel execution
    git-svn-id: http://galileo.dmi.unict.it/svn/relational/trunk@174 014f5005-505e-4b48-8d0a-63407b615a7c

---
+++ relational/parallel.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Relational
+# Copyright (C) 2009  Salvo "LtWorf" Tomaselli
+# 
+# Relation is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
+
+'''This module offers capability of executing relational queries in parallel.'''
+
+def weight (n):
+    '''This function returns a weight that tries to give an approssimation of the
+    time that will be required to execute the expression'''
+    pass
