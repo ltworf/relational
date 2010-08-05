557fcabb010b6911f2c29460f7cfa33216792363
LtWorf <LtWorf@014f5005-505e-4b48-8d0a-63407b615a7c>
2010-08-05 15:44:52 +0000

    - Added in trunk debian/ dir to build the debian package. It won't be added in the source tar.gz
    git-svn-id: http://galileo.dmi.unict.it/svn/relational/trunk@248 014f5005-505e-4b48-8d0a-63407b615a7c

---
+++ debian/relational-cli.setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name='relational_readline', packages=('relational_readline',))
---
+++ debian/relational.setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name='relational_gui', packages=('relational_gui',))
