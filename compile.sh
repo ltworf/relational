e9710f28cdef3eda6e78a23da661104081340e05
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2015-03-28 17:43:35 +0100

    Fix wrong redirect operator

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 for i in pages/*
 do
-    php templates/header >> $(basename $i).html
+    php templates/header > $(basename $i).html
     php $i >> $(basename $i).html
     php templates/footer >> $(basename $i).html
     git add $(basename $i).html
