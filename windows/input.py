f76c1bf3e23ac150e01dd1b3d30b90e698733ca1
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2015-06-04 21:51:20 +0200

    Changed version in input.py
    Changed the version in the input.py to create relational.exe

--- windows/input.py
+++ windows/input.py
@@ -3,20 +3,20 @@
 # coding=UTF-8
 # Relational
 # Copyright (C) 2008  Salvo "LtWorf" Tomaselli
-# 
+#
 # Relational is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 # author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
 
 from distutils.core import setup
@@ -37,5 +37,5 @@ setup(options = {
           {"script": "relational_gui.py","icon_resources": [(0, "windows/favicon.ico")]}
           ]
       ,name="Relational",
-               version="1.2"
+               version="2.0"
       )
